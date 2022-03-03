2c16cfd4e5d1a939458236e66c80be0a44aa388b
TwentyFive <TwentyFiveSoftware@gmail.com>
2022-03-03 16:53:20 +0100

    fix

--- src/modules/game/gameManager.ts
+++ src/modules/game/gameManager.ts
@@ -6,7 +6,7 @@ import { SocketResponseType } from '../../types/SocketResponseType';
 import { chooseRandomText } from '../text/textManager';
 import { sendLobbyUpdate } from '../lobby/lobbyManager';
 
-const GAME_START_COUNTDOWN_SECONDS = 5.5;
+const GAME_START_COUNTDOWN_SECONDS = 5.99;
 
 let games: IGame[] = [];
 
